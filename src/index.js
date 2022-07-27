a5ff98d2fe32601ef0f99f93151b49e14be9e4c0
AnKhly <91781956+AnKhly@users.noreply.github.com>
2022-07-27 22:33:09 +0300

    feat:add result task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	let stack = [];
+	let breckets = {
+		')': '(',
+		'}': '{',
+		']': '[',
+		'|': '|',
+	}
+	for (let index = 0; index < str.length; index++) {
+		const el = str[index];
+		if (closeBrack(el)) {
+			if (breckets[el] !== stack.pop()) {
+				return false;
+			}
+		} else {
+			stack.push(el);
+		}
+	}
+	if (stack.length === 0) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+function closeBrack(ch) {
+	if ([')', '}', ']',].indexOf(ch) !== -1) {
+		return true;
+	}
 }
